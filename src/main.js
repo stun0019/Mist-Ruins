dc9e160b81f346bf0c4f78513178ca0af6908776
stun0019 <ootdacc519@gmail.com>
2026-08-07 10:59:26 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+import { Game } from "./game.js";
 
+const container = document.querySelector("#game-container");
+
+if (!container) {
+  throw new Error("找不到 #game-container。");
+}
+
+const game = new Game(container);
+
+game.start();
